f22caeb8c3b080d0d52b696ad076d7c98e2a64b2
Maayan Kfir <mayankfir@gmail.com>
2018-09-21 18:52:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,9 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
 else if
-  phrase.uppercase
+  phrase.upcase
   "HUH?! SPEAK UP, SONNY!"
 end
-
 
 end
 end
